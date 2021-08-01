5db365406261ab5792f920bedc29689c320d3b62
liyuan <754070069@qq.com>
2021-08-01 17:44:14 +0800

    "System.out.println("0724增加了");"

--- ssm-crud/src/main/java/com/atguigu/crud/test/TestGit.java
+++ ssm-crud/src/main/java/com/atguigu/crud/test/TestGit.java
@@ -5,5 +5,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("222");
+        System.out.println("0724增加了");
+
     }
 }
